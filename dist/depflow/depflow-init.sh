4457cbc9cca51665a303051cd20e4bbc6041e3ac
obetoferreira <obetoferreira@gmail.com>
2015-12-09 21:20:08 -0200

    Replacing depflow init

--- dist/depflow/depflow-init.sh
+++ dist/depflow/depflow-init.sh
@@ -2,11 +2,11 @@
 echo 'Creating .depflowconfig file...';
 touch .depflowconfig;
 echo 'default=(	
-	"port:55851" 
-	"usr:hquartel" 
-	"host:quarteldesign.com" 
+	"port:22" 
+	"usr:<insert-username-here>" 
+	"host:<insert-host-here>" 
 	"fldr:<insert-folder-name-here>" 
 	"repo:<insert-repo-url-here>" 
-	"branch:staging"
+	"branch:master"
 )' > .depflowconfig;
 echo "Done. Don't forget to fill the .depflowconfig file with your info.";
